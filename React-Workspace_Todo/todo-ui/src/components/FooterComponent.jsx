12d2aaaf34c5fb8a7b3516aa14ee49255e6c6c36
unknown <aksm675@gmail.com>
2024-11-17 11:22:24 +0530

    Uudemy project added

---
+++ React-Workspace_Todo/todo-ui/src/components/FooterComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className='footer'>
+              <p className='text-center'>Copyrights reserved at 2023-25 by Akash Kadam</p>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
